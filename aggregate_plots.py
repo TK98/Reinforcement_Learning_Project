bbe47562064306989c82c44563db4e084b5ac308
Xiaojin Gu <feibusam@hotmail.com>
2020-10-22 22:16:24 +0200

    Add options to plot loss and training time

--- aggregate_plots.py
+++ aggregate_plots.py
@@ -12,6 +12,9 @@ save_dir = ''
 
 
 def plot(data_list):
+    """
+    Plot the data
+    """
     sns.set_style(style='darkgrid')
     data_tuples = [item for idx, item in enumerate(data_list)]
 
@@ -23,8 +26,16 @@ def plot(data_list):
 
         sns_plot = sns.lineplot(data=data, color=sns.color_palette(), ci=80)
 
-        if plot_mode == 'time':
+        if plot_mode == 'reward':
+            y_label = 'Reward per episode'
+        elif plot_mode == 'loss':
+            y_label = 'Average loss per episode'
+        elif plot_mode == 'time':
+            y_label = f'Training time per episode in seconds'
+            print('Mean')
             print(data.mean().round(2))
+            print('Std')
+            print(data.std().round(2))
 
         leg_lines = sns_plot.legend().get_lines()
 
@@ -37,7 +48,7 @@ def plot(data_list):
 
         sns_plot.set_title(f'Training {plot_mode} over episodes for {title}')
         sns_plot.set_xlabel("Episodes")
-        sns_plot.set_ylabel(f'Training {plot_mode} per episode{" in seconds" if plot_mode == "time" else ""}')
+        sns_plot.set_ylabel(y_label)
 
         # sns_plot.set(ylim=(-100, 0))
 
@@ -47,7 +58,10 @@ def plot(data_list):
         print('Plot created.')
 
 
-def aggregate_data(files, file_name, semi_gradient, env_name, net_name):
+def aggregate_data(files, semi_gradient, env_name, net_name):
+    """
+    Aggregate data from multiple files into one dataframe
+    """
     gradient_mode = 'Semi-gradient' if semi_gradient else 'Full-gradient'
     data = []
 
@@ -55,22 +69,27 @@ def aggregate_data(files, file_name, semi_gradient, env_name, net_name):
         with open(file, 'rb') as f:
             p = pickle.load(f)
 
-            if plot_mode == "reward":
+            if plot_mode == 'reward':
                 data.append(p['train']['episode_rewards'])
-            elif plot_mode == "time":
-                 data.append([p['train']['duration']])
+            elif plot_mode == 'loss':
+                data.append(p['train']['losses'])
+            elif plot_mode == 'time':
+                data.append([p['train']['duration']])
 
     df = pd.DataFrame(data)
 
-    if plot_mode == "reward":
-        df = df.T
+    if plot_mode != "time": # time is a 1-D list, does not need to be transposed
+        df = df.T 
 
     df.columns=[f'{gradient_mode} {net_name}' for i in range(df.shape[1])]
 
     return df
 
 
-def get_files(env, net, batch_size, discount_factor, semi_gradient, layer, lr, lr_step_size, lr_gamma, replay_memory, config):
+def get_matched_files(env, net, batch_size, discount_factor, semi_gradient, layer, lr, lr_step_size, lr_gamma, replay_memory, config):
+    """
+    Returns the files that match the config file
+    """
     for num_episodes in config[ex.TRAIN_EPS_KEY]:
         file_name, _ = ex.get_file_name_and_config(env=env,
                                                    net=net,
@@ -85,29 +104,35 @@ def get_files(env, net, batch_size, discount_factor, semi_gradient, layer, lr, l
                                                    num_episodes=num_episodes,
                                                    seed="*")
 
+        # escape square brackets for glob
         file_name = file_name.replace('[', ':left:').replace(']', '[]]').replace(':left:', '[[]')
         files = glob(f'{save_dir}/{file_name}.pkl')
 
-        return file_name, files
+        return files
 
 
-def do_stuff(env, net, batch_size, discount_factor, semi_gradient, layer, lr, lr_step_size, lr_gamma, rep_mem, config):
-    file_name, files = get_files(env, net, batch_size, discount_factor, semi_gradient, layer, lr, lr_step_size, lr_gamma, rep_mem, config)
+def get_aggregated_data(env, net, batch_size, discount_factor, semi_gradient, layer, lr, lr_step_size, lr_gamma, rep_mem, config):
+    """
+    Find the matched files and aggregate data
+    """
+    files = get_matched_files(env, net, batch_size, discount_factor, semi_gradient, layer, lr, lr_step_size, lr_gamma, rep_mem, config)
 
     if files:
-        return env.__name__, aggregate_data(files, file_name, semi_gradient, env.__name__, net.__name__)
+        return env.__name__, aggregate_data(files, semi_gradient, env.__name__, net.__name__)
 
 def main(config):
-    data_list = ex.do_loop(config, do_stuff)
-    plot(data_list)
+    data = ex.do_loop(config, get_aggregated_data)
+    plot(data)
 
 
 if __name__ == "__main__":
-    config_filename = 'experiments_config_cartpole.json'
-    save_dir = "saved_experiments_v2"
+    config_filename = 'experiments_config_acrobot.json'
+    save_dir = "saved_experiments"
     config = ex.load_config(config_filename)
 
-    plot_mode = "time" # reward or time
+    plot_mode = "time"
+
+    assert plot_mode in ['reward', 'time', 'loss'], "plot_mode must be in ['reward', 'time', 'loss']"
 
     main(config)
 
